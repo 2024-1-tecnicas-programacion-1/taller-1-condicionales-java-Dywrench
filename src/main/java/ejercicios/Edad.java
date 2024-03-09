3251dc61853775ec8eb6653afc4f3e41e5c5b2df
Dywrench <jv88611@gmail.com>
2024-03-09 17:58:10 -0500

    Ejercicio Edad-java Juan Pablo Velasquez Hernandez

--- src/main/java/ejercicios/Edad.java
+++ src/main/java/ejercicios/Edad.java
@@ -2,8 +2,8 @@
 package ejercicios;
 
 import static ejercicios.SetDeTenis.evaluar;
-import java.util.Scanner;
-
+import  java.util.Scanner;
+import  java.time.LocalDate;
 /**
  *
  * @author danielsanchez
@@ -11,7 +11,18 @@ import java.util.Scanner;
 public class Edad {
     public static String evaluar(int dia, int mes, int anno) {
         // TODO: Coloca aquí el código del ejercicio 6: Edad
-        return "";
+         LocalDate currentDate =LocalDate.now();
+         int dia_act = currentDate.getDayOfMonth();
+         int mes_act = currentDate.getMonthValue();
+         int anno_act = currentDate.getYear();
+         int edad=anno_act-anno;
+         double mesesleidos_a_dias=mes*30.44;
+         double mesesactuales_a_dias=mes_act*30.44;
+         if (mes_act < mes || (mes_act == mes && dia_act < dia)) {
+            edad--;
+         }
+         String edad_texto = new Integer(edad).toString();
+        return edad_texto;
     }
     
     public static void main(String[] args) {
@@ -25,6 +36,6 @@ public class Edad {
         int anno = lector.nextInt();
         
         String respuesta = evaluar(dia, mes, anno);
-        System.out.println(respuesta);
+        System.out.println("Usted tiene "+respuesta+" años");
     }
 }
