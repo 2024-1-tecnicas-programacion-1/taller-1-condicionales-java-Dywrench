b868f1163b9b77951f4ac2632ec9c76b56598531
Dywrench <jv88611@gmail.com>
2024-03-09 16:12:06 -0500

    Ejercicio IMC-Java Juan Pablo Velasquez Hernandez

--- src/main/java/ejercicios/IMC.java
+++ src/main/java/ejercicios/IMC.java
@@ -11,7 +11,18 @@ import java.util.Scanner;
 public class IMC {
     public static String evaluar(int peso, double estatura, int edad) {
         // TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-        return "";
+        String resultado="";
+         double IMC = peso / (estatura * estatura);
+           if (edad<45 && IMC < 22.0) {
+            resultado = "bajo";
+        } else if (IMC >= 22.0 && edad < 45) {
+            resultado = "medio";
+        } else if (IMC < 22.0 && edad >= 45) {
+            resultado = "medio";
+        } else if (IMC >= 22.0 && edad >= 45) {
+            resultado = "alto";
+        }
+        return resultado;
     }
     
     public static void main(String[] args) {
